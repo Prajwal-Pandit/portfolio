ba98beba6247e64ea512ccbc4102d9782bb75638
Sushant <prajwal.pandit7@gmail.com>
2021-05-22 15:48:48 +0530

    email test

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,3 +181,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+/*==================== EMAIL SERVICE ====================*/ 
